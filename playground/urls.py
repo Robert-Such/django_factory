4b8654b447d34fe79905240f7a54902e3addce9c
Robert Such <85602363+Robert-Such@users.noreply.github.com>
2022-03-31 00:39:47 -0400

    cleaned up data models.  added dummy data

--- playground/urls.py
+++ playground/urls.py
@@ -4,4 +4,4 @@ from . import views
 # URLConf
 urlpatterns = [
     path('hello/', views.say_hello)
-    ]
+]
--- storefront/settings.py
+++ storefront/settings.py
@@ -84,20 +84,13 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'storefront',
-#         'HOST': 'localhost',
-#         'USER': 'root',
-#         'PASSWORD': 'MyPassword'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'MyPassword'
     }
 }
 
